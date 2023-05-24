963cc3fbba658a7710cc3ab0c413dfb3e294212c
Jonas Morales <josuemodev@gmail.com>
2023-05-24 16:34:13 -0600

    eslint, prettier and husky have added

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,53 @@
 module.exports = {
-  env: { browser: true, es2020: true },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  env: {
+    browser: true,
+    es2021: true
+  },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'standard-with-typescript',
+    'plugin:prettier/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended'
   ],
+  overrides: [],
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: 'tsconfig.json',
+    ecmaFeatures: {
+      jsx: true
+    }
+    // tsconfigRootDir: __dirname,
   },
+  ignorePatterns: ['.eslintrc.cjs'],
+  plugins: [
+    'react',
+    'prettier',
+    'jsx-a11y',
+    'react-hooks',
+    'react-refresh',
+    'promise',
+    'n',
+    'import'
+  ],
+  rules: {
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'no-empty-function': 'off',
+    eqeqeq: ['error', 'always'],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'import/no-absolute-path': 'off',
+    '@typescript-eslint/no-duplicate-enum-values': 'warn',
+    '@typescript-eslint/no-floating-promises': 'off',
+    '@typescript-eslint/triple-slash-reference': 'off'
+  }
 }
